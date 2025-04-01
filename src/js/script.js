bb563b5e2532f516b25b83a0e3abf3532117d80c
Joao <joaopedroaraujostellare@gmail.com>
2025-04-01 09:10:58 -0300

    DECLARAÇOES E VARIAVEIS

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+//DECLARAÇÕES E VARIAVEIS
+var nome ="fiap";
+console.log(nome)
+
+let idade =18;
+console.log(idade)
+
+const sobrenome ="Cidade";
+console.log(sobrenome)
+//undefined
+let aula;
+console.log(aula)
+//null
+let valor = null;
+console.log(valor)
+
+let exemplo1={};
+
+let exemplo2=[];
+
