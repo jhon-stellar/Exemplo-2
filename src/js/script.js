754d43bc52218bbb92fc7478b219834ac818c7dd
Joao <joaopedroaraujostellare@gmail.com>
2025-04-01 09:27:57 -0300

    adicionando comiçoes

--- src/js/script.js
+++ src/js/script.js
@@ -29,9 +29,23 @@ let exemplo5 =true;
 console.log(typeof exemplo5)
 
 let exemplo6 =["huguinho","zezinho","luizinho"];
-console.log(typeof exemplo5)
-
+console.log(typeof exemplo6)
 
 //CONVERÇÕES
 
+//float => inteiro
+let numFloat =123.456;
+console.log(parseInt(numFloat))
+
+//string => float
+let numString ="567.891";
+console.log(parseFloat(numString))
+
+//float => string
+
+let numFloat1 =663.112;
+console.log(numFloat.toString());
+
+let numInt=100;
+console.log(numInt.toString())
 
